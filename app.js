fd8d79144388cb07d4c4052ed99dc97e7b96ae3d
jaejunmin <jaejunmi@usc.edu>
2019-05-01 22:01:14 -0700

    test

--- app.js
+++ app.js
@@ -1,54 +1,112 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
-// const Artist = require('./models/artist');
-// const Playlist = require('./models/playlist');
-// const Album = require('./models/album');
-const Track = require('./models/track');
-// const {Op} = Sequelize;
+const Student = require('./models/student');
+const Comment = require('./models/comment');
+const College = require('./models/college');
+const Major = require('./models/major');
+const { Op } = Sequelize;
 const app = express();
+
 app.use(bodyParser.json());
 
-let check  =6 ;
+Student.belongsTo(College,{
+  foreignKey: 'college_id'
+});
+College.hasMany(Student,{
+  foreignKey: 'college_id'
+});
+Student.belongsTo(Major,{
+  foreignKey: 'major_id'
+});
+Major.hasMany(Student,{
+  foreignKey: 'major_id'
+});
+Student.hasMany(Comment,{
+  foreignKey: 'student_id'
+})
+Comment.belongsTo(Student,{
+  foreignKey: 'student_id'
+});
+Major.belongsToMany(College,{
+  through: 'college_major',
+  foreignKey: 'major_id',
+  timestamps: false
+});
+College.belongsToMany(Major,{
+  through: 'college_major',
+  foreignKey: 'college_id',
+  timestamps: false
+});
 
-// Artist.hasMany(Album,{
-//   foreignKey: 'ArtistId'
-// });
-//
-// Album.belongsTo(Artist,{
-//   foreignKey: 'ArtistId'
-// });
-//
-// Playlist.belongsToMany(Track, {
-//   through: 'playlist_track',
-//   foreignKey: 'PlaylistId',
-//   timestamps: false
 
-app.get('/api/tracks/:id', function(request, response){
+app.delete('/api/comments/:id', function(request, response) {
   let { id } = request.params;
-  Track.findByPk(id,{
-  }).then((track)=>{
-    if (track){
-      response.json(track);
-    }else{
+
+  Comment
+    .findByPk(id)
+    .then((comment) => {
+      if (comment) {
+          return comment.destroy();
+      } else {
+        return Promise.reject();
+      }
+    })
+    .then(() => {
+      response.status(204).send();
+    }, () => {
       response.status(404).send();
-    }
+    });
+});
+
+app.post('/api/comments', function(request, response) {
+  Comment.create({
+    title: request.body.title,
+    content:request.body.content,
+    student_id: 1,
+  }).then((comment) => {
+    response.json(comment);
+  }, (validation) => {
+    response.status(422).json({
+      errors: validation.errors.map((error) => {
+        return {
+          attribute: error.path,
+          message: error.message
+        };
+      })
+    });
   });
 });
-app.patch('/api/tracks/:id', function(request, response){
+app.get('/api/comments', function(request, response) {
+  let filter = {};
+  let { q } = request.query;
+  if (q) {
+    filter = {
+      where: {
+        title: {
+          [Op.like]: `${q}%`
+        }
+      }
+    };
+  }
+  Comment.findAll(filter).then((comments) => {
+    response.json(comments);
+  });
+});
+app.patch('/api/comments/:id', function(request, response){
   let { id } = request.params;
   // console.log(id)
 
- Track.findByPk(id).then((track)=>{
+ Comment.findByPk(id).then((track)=>{
     if (track){
     }else{
       return Promise.reject();
     }
   }).then(()=>{
-    Track.update({
-      name: request.body.name,
-      milliseconds: request.body.milliseconds,
-      unitPrice: request.body.unitPrice},
+    Comment.update({
+      title: request.body.title,
+      content: request.body.content,
+      student_id: request.body.student_id},
       {
         where:{ id : request.params.id }
     }).then(()=>{
@@ -63,13 +121,52 @@ app.patch('/api/tracks/:id', function(request, response){
           })
         })
     });
-
   },()=>{
       response.status(404).send();
   });
-
-
 });
+
+app.get('/api/comments/:id', function(request, response) {
+  let { id } = request.params;
+
+  Comment.findByPk(id, {
+    include: [Student]
+  }).then((comment) => {
+    if (comment) {
+      response.json(comment);
+    } else {
+      response.status(404).send();
+    }
+  });
+});
+app.get('/api/students/:id', function(request, response) {
+  let { id } = request.params;
+
+  Student.findByPk(id).then((comment) => {
+    if (comment) {
+      response.json(comment);
+    } else {
+      response.status(404).send();
+    }
+  });
+});
+app.get('/api/students', function(request, response) {
+  let filter = {};
+  let { q } = request.query;
+  if (q) {
+    filter = {
+      where: {
+        title: {
+          [Op.like]: `${q}%`
+        }
+      }
+    };
+  }
+  Student.findAll(filter).then((comments) => {
+    response.json(comments);
+  });
+});
+
 
 
 app.listen(process.env.PORT || 8000);
